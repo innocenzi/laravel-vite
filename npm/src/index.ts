0dbfbf1aae042757f43a2cbdb1b4e944b6b04b93
Enzo Innocenzi <enzo@innocenzi.dev>
2021-02-20 19:28:15 +0100

    style: add eslint

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,10 +1,17 @@
-import { Plugin, UserConfig } from "vite";
-import deepmerge from "deepmerge";
-import execa from "execa";
-import path from "path";
-import fs from "fast-glob";
-import chalk from 'chalk';
-import dotenv from 'dotenv';
+import path from 'path'
+import { Plugin, UserConfig } from 'vite'
+import deepmerge from 'deepmerge'
+import execa from 'execa'
+import fs from 'fast-glob'
+import chalk from 'chalk'
+import dotenv from 'dotenv'
+
+interface PhpConfiguration {
+	build_path: string
+	dev_url: string
+	entrypoints: false | string | string[]
+	aliases: Record<string, string>
+}
 
 /**
  * A plugin enabling HMR for Blade files.
@@ -17,74 +24,74 @@ export const laravel = (): Plugin => ({
 		if (file.endsWith('.blade.php')) {
 			server.ws.send({
 				type: 'full-reload',
-				path: '*'
-			});
+				path: '*',
+			})
 		}
-	}
+	},
 })
 
 export class ViteConfiguration {
-	public publicDir: string;
-	public build: UserConfig["build"];
-	public server: UserConfig["server"];
-	public plugins: UserConfig["plugins"];
-	public base: UserConfig["base"];
-	public resolve: UserConfig["resolve"];
+	public publicDir: string
+	public build: UserConfig['build']
+	public server: UserConfig['server']
+	public plugins: UserConfig['plugins']
+	public base: UserConfig['base']
+	public resolve: UserConfig['resolve']
 
 	constructor(config?: PhpConfiguration) {
-		dotenv.config();
-		this.base = process.env.ASSET_URL ?? '/';
-		this.publicDir = "resources/static";
+		dotenv.config()
+		this.base = process.env.ASSET_URL ?? '/'
+		this.publicDir = 'resources/static'
 		this.build = {
 			manifest: true,
 			outDir: config?.build_path
 				? `public/${config.build_path}`
-				: "public/build",
+				: 'public/build',
 			rollupOptions: {
 				input: [],
 			},
-		};
+		}
 
 		this.plugins = [
-			laravel()
-		];
+			laravel(),
+		]
 
 		if (config?.aliases) {
 			this.resolve = {
 				alias: Object.fromEntries(
 					Object.entries(config.aliases).map(([alias, directory]) => {
-						return [alias, path.join(process.cwd(), directory)];
-					})
-				)
-			};
+						return [alias, path.join(process.cwd(), directory)]
+					}),
+				),
+			}
 
-			generateAliases();
+			generateAliases()
 		}
 
 		if (config?.dev_url) {
-			const [protocol, host, port] = config.dev_url.split(":");
+			const [protocol, host, port] = config.dev_url.split(':')
 			this.server = {
 				host: host.substr(2),
-				https: protocol === "https",
+				https: protocol === 'https',
 				port: port ? Number(port) : 3000,
-			};
+			}
 		}
 
 		if (config?.entrypoints) {
 			const directories = !Array.isArray(config?.entrypoints)
 				? [config?.entrypoints]
-				: config?.entrypoints;
+				: config?.entrypoints
 
 			const matches = fs.sync(
-				directories.map((directory) => `${directory}/*`),
+				directories.map(directory => `${directory}/*`),
 				{
 					onlyFiles: true,
 					globstar: false,
 					dot: false,
-				}
+				},
 			);
 
-			(this.build.rollupOptions!.input! as string[]).push(...matches);
+			(this.build.rollupOptions!.input! as string[]).push(...matches)
 		}
 	}
 
@@ -93,8 +100,8 @@ export class ViteConfiguration {
 	 * Defaults to resources/static.
 	 */
 	public withStaticAssets(publicDir: string): this {
-		this.publicDir = publicDir;
-		return this;
+		this.publicDir = publicDir
+		return this
 	}
 
 	/**
@@ -102,8 +109,8 @@ export class ViteConfiguration {
 	 * Defaults to public/build.
 	 */
 	public withOutput(outDir: string): this {
-		this.build!.outDir = outDir;
-		return this;
+		this.build!.outDir = outDir
+		return this
 	}
 
 	/**
@@ -111,15 +118,15 @@ export class ViteConfiguration {
 	 *
 	 * @example
 	 * export default createViteConfiguration()
-	 *  .withEntry("resources/js/app.js")
+	 *	.withEntry("resources/js/app.js")
 	 */
 	public withEntry(...entries: string[]): this {
 		this.build!.rollupOptions!.input = [
 			...(this.build!.rollupOptions!.input as string[]),
 			...entries,
-		];
+		]
 
-		return this;
+		return this
 	}
 
 	/**
@@ -127,43 +134,35 @@ export class ViteConfiguration {
 	 *
 	 * @example
 	 * export default createViteConfiguration()
-	 *  .withEntries("resources/js/app.js", "resources/js/admin.js")
+	 *	.withEntries("resources/js/app.js", "resources/js/admin.js")
 	 */
 	public withEntries(...entries: string[]): this {
-		return this.withEntry(...entries);
+		return this.withEntry(...entries)
 	}
 
 	/**
 	 * Merges in the given Vite configuration.
 	 */
 	public merge(config: UserConfig): this {
-		const result: UserConfig = deepmerge(this, config);
+		const result: UserConfig = deepmerge(this, config)
 
 		for (const [key, value] of Object.entries(result)) {
-			if (key === 'base') {
-				console.warn(chalk.yellow.bold('(!) "base" option should not be used with Laravel Vite. Use the "ASSET_URL" environment variable instead.'));
-			}
+			if (key === 'base')
+				console.warn(chalk.yellow.bold('(!) "base" option should not be used with Laravel Vite. Use the "ASSET_URL" environment variable instead.'))
 
 			// @ts-expect-error
-			this[key] = value;
+			this[key] = value
 		}
 
-		return this;
+		return this
 	}
-}
-
-interface PhpConfiguration {
-	build_path: string;
-	dev_url: string;
-	entrypoints: false | string | string[];
-	aliases: Record<string, string>;
 }
 
 /**
  * Calls an artisan command.
  */
 function callArtisan(...params: string[]): string {
-	return execa.sync("php", ["artisan", ...params])?.stdout;
+	return execa.sync('php', ['artisan', ...params])?.stdout
 }
 
 /**
@@ -171,10 +170,11 @@ function callArtisan(...params: string[]): string {
  */
 function generateAliases() {
 	try {
-		callArtisan("vite:aliases");
-	} catch (error) {
-		console.warn("Could not regenerate tsconfig.json.");
-		console.error(error);
+		callArtisan('vite:aliases')
+	}
+	catch (error) {
+		console.warn('Could not regenerate tsconfig.json.')
+		console.error(error)
 	}
 }
 
@@ -183,10 +183,11 @@ function generateAliases() {
  */
 function getConfigurationFromArtisan(): PhpConfiguration | undefined {
 	try {
-		return JSON.parse(callArtisan("vite:config")) as PhpConfiguration;
-	} catch (error) {
-		console.warn("Could not read configuration from PHP.");
-		console.error(error);
+		return JSON.parse(callArtisan('vite:config')) as PhpConfiguration
+	}
+	catch (error) {
+		console.warn('Could not read configuration from PHP.')
+		console.error(error)
 	}
 }
 
@@ -197,7 +198,7 @@ function getConfigurationFromArtisan(): PhpConfiguration | undefined {
  * @see https://github.com/innocenzi/laravel-vite
  */
 export function createViteConfiguration() {
-	return new ViteConfiguration(getConfigurationFromArtisan());
+	return new ViteConfiguration(getConfigurationFromArtisan())
 }
 
-export default createViteConfiguration();
+export default createViteConfiguration()
--- npm/vite.config.ts
+++ npm/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import path from "path";
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "index.ts"),
-            name: "laravel-vite",
-            formats: ["es"],
-        },
-    },
-});
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'index.ts'),
+			name: 'laravel-vite',
+			formats: ['es'],
+		},
+	},
+})
