82ea414f1e321612174ab4e8b2cdca33aad34464
Enzo Innocenzi <enzo@innocenzi.dev>
2021-02-23 21:55:41 +0100

    fix: support entrypoints by name in production

--- src/Exceptions/NoSuchEntrypointException.php
+++ src/Exceptions/NoSuchEntrypointException.php
@@ -6,6 +6,6 @@ class NoSuchEntrypointException extends \Exception
 {
     public function __construct(string $entry)
     {
-        $this->message = "${entry} does not exist in the manifest. Make sure you added an entry point in the Vite configuration.";
+        $this->message = "'${entry}' does not exist in the manifest. Make sure you added an entry point in the Vite configuration.";
     }
 }
