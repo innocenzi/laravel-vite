152ecc9e33de1def28e23e1a99badcd0bb488db3
Enzo Innocenzi <enzo@innocenzi.dev>
2021-02-23 18:10:54 +0100

    test: specify type attributes
    Related: https://github.com/innocenzi/laravel-vite/commit/42d3c955db814384c7282d4cf8c6261f17094639

--- tests/Unit/ViteTest.php
+++ tests/Unit/ViteTest.php
@@ -33,13 +33,13 @@ it('throws when generating a non-existing entry script in a production environme
 it('generates an entry script in a production environment', function () {
     set_env('production');
     expect(get_vite()->getEntry('resources/js/app.js'))
-        ->toEqual('<script src="http://localhost/build/app.83b2e884.js"></script>');
+        ->toEqual('<script type="module" src="http://localhost/build/app.83b2e884.js"></script>');
 });
 
 it('generates scripts and css from an entry point in a production environment', function () {
     set_env('production');
     expect(get_vite('with_css.json')->getEntry('resources/js/app.js'))
-        ->toEqual('<script src="http://localhost/build/app.83b2e884.js"></script><link rel="stylesheet" href="http://localhost/build/app.e33dabbf.css" />');
+        ->toEqual('<script type="module" src="http://localhost/build/app.83b2e884.js"></script><link rel="stylesheet" href="http://localhost/build/app.e33dabbf.css" />');
 });
 
 it('finds an entrypoint by its name when its directory is registered in the configuration', function () {
@@ -77,7 +77,7 @@ it('does not generate client script tag in production environment', function () 
     Config::set('vite.entrypoints', 'scripts');
     App::setBasePath(__DIR__);
     expect(get_vite()->getClientAndEntrypointTags())
-        ->toEqual('<script src="http://localhost/build/app.83b2e884.js"></script>');
+        ->toEqual('<script type="module" src="http://localhost/build/app.83b2e884.js"></script>');
 });
 
 it('generates production URLs that take the ASSET_URL environment variable into account', function () {
@@ -90,5 +90,5 @@ it('generates production URLs that take the ASSET_URL environment variable into 
     Config::set('vite.entrypoints', 'scripts');
     App::setBasePath(__DIR__);
     expect(get_vite()->getClientAndEntrypointTags())
-        ->toEqual('<script src="https://cdn.random.url/build/app.83b2e884.js"></script>');
+        ->toEqual('<script type="module" src="https://cdn.random.url/build/app.83b2e884.js"></script>');
 });
