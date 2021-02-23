fa2db4f1ec7534feb14e60ead6f72b035fe372b0
René Sinnbeck <rene.sinnbeck@gmail.com>
2021-02-23 17:46:39 +0100

    feat: support php 7.4 (#21)
    Co-authored-by: René Sinnbeck <rs@aalund.com>

--- src/Manifest.php
+++ src/Manifest.php
@@ -41,10 +41,11 @@ class Manifest implements Htmlable, Stringable
      */
     public function getEntry(string $entry): ManifestEntry
     {
-        return $this->entries->get(
-            $entry,
-            fn () => throw new NoSuchEntrypointException($entry)
-        );
+        if (! $this->entries->has($entry)) {
+            throw new NoSuchEntrypointException($entry);
+        }
+
+        return $this->entries->get($entry);
     }
 
     /**
