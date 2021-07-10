93e7bed1908d823db9878498202d047b9f92f0e9
Enzo Innocenzi <enzo@innocenzi.dev>
2021-07-10 19:07:16 +0200

    chore: update ping_timeout config comment

--- config/vite.php
+++ config/vite.php
@@ -43,7 +43,9 @@ return [
     |--------------------------------------------------------------------------
     | The maximum duration, in seconds, that the ping to the development
     | server should take while trying to determine whether to use the
-    | manifest or the server in a local environment.
+    | manifest or the server in a local environment. Using false will disable
+    | the feature.
+    | https://laravel-vite.innocenzi.dev/guide/configuration.html#ping-timeout
     */
     'ping_timeout' => .1,
 
