19468b7e62293a49a1476b59a2964eddfb5cbc9b
Gennaro Landolfi <gennarolandolfi@codedwork.it>
2021-08-27 18:18:53 +0200

    refactor: use `afterResolving` instead of Blade facade (#97)

--- src/ViteServiceProvider.php
+++ src/ViteServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Innocenzi\Vite;
 
-use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
 use Innocenzi\Vite\Commands\ExportConfigurationCommand;
 use Innocenzi\Vite\Commands\GenerateAliasesCommand;
 use Spatie\LaravelPackageTools\Package;
@@ -19,24 +19,26 @@ class ViteServiceProvider extends PackageServiceProvider
             ->hasCommand(GenerateAliasesCommand::class);
     }
 
-    public function registeringPackage()
+    public function bootingPackage()
     {
         $this->app->singleton(Vite::class, fn () => new Vite());
 
-        Blade::directive('vite', function ($entryName = null) {
-            if (! $entryName) {
-                return '<?php echo vite_tags() ?>';
-            }
+        $this->app->afterResolving('blade.compiler', function (BladeCompiler $compiler) {
+            $compiler->directive('vite', function ($entryName = null) {
+                if (! $entryName) {
+                    return '<?php echo vite_tags() ?>';
+                }
 
-            return sprintf('<?php echo vite_entry(e(%s)); ?>', $entryName);
-        });
+                return sprintf('<?php echo vite_entry(e(%s)); ?>', $entryName);
+            });
 
-        Blade::directive('client', function () {
-            return '<?php echo vite_client(); ?>';
-        });
+            $compiler->directive('client', function () {
+                return '<?php echo vite_client(); ?>';
+            });
 
-        Blade::directive('react', function () {
-            return '<?php echo vite_react_refresh_runtime(); ?>';
+            $compiler->directive('react', function () {
+                return '<?php echo vite_react_refresh_runtime(); ?>';
+            });
         });
     }
 }
