e763c71513a516f6ee94510dc1ecb938fd6822fa
Enzo Innocenzi <enzo@innocenzi.dev>
2021-10-03 20:22:09 +0200

    feat: export config
    Closes #137

--- src/Commands/ExportConfigurationCommand.php
+++ src/Commands/ExportConfigurationCommand.php
@@ -3,18 +3,29 @@
 namespace Innocenzi\Vite\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Innocenzi\Vite\Vite;
 
 class ExportConfigurationCommand extends Command
 {
-    public $signature = 'vite:config';
+    public $signature = 'vite:config {--export= : Path to a file to write the configuration into.}';
     public $description = 'Prints the Vite configuration.';
     public $hidden = true;
 
     public function handle()
     {
-        $this->output->write($this->getConfigurationAsJson());
+        $config = $this->getConfigurationAsJson();
+
+        if ($path = $this->option('export')) {
+            File::put($path, $config);
+
+            $this->info("Configuration file written to <comment>${path}</comment>.");
+
+            return;
+        }
+
+        $this->output->write($config);
     }
 
     public function getConfigurationAsJson(): string
--- tests/Features/ExportConfigurationCommandTest.php
+++ tests/Features/ExportConfigurationCommandTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Innocenzi\Vite\Commands\ExportConfigurationCommand;
 
 it('exposes the configuration contents to the command line', function () {
@@ -7,5 +8,16 @@ it('exposes the configuration contents to the command line', function () {
 
     test()->artisan('vite:config')
         ->expectsOutput($output)
+        ->assertExitCode(0);
+});
+
+it('writes the configuration contents to the file system if requested', function () {
+    File::partialMock()->shouldReceive('put')->withArgs([
+        'vite.config.json',
+        app(ExportConfigurationCommand::class)->getConfigurationAsJson(),
+    ]);
+
+    test()->artisan('vite:config --export=vite.config.json')
+        ->expectsOutput('Configuration file written to vite.config.json.')
         ->assertExitCode(0);
 });
