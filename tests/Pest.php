fda2207ac4fcb69680dfea8206ba6068ad10832f
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-29 21:18:46 +0100

    test: clean-up previous test suite

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Innocenzi\Vite\Tests\TestCase;
-use Innocenzi\Vite\Vite;
 use Pest\TestSuite;
-use Symfony\Component\Process\Process;
 
 uses(Innocenzi\Vite\Tests\TestCase::class)->in('Unit');
 uses(Innocenzi\Vite\Tests\TestCase::class)->in('Features');
@@ -41,66 +39,20 @@ function this(): TestCase
  */
 function set_env(string $env): void
 {
-    App::bind('env', fn () => $env);
-}
-
-function get_vite(string $manifest = 'manifest.json'): Vite
-{
-    return new Vite(__DIR__ . "/Unit/manifests/${manifest}");
+    app()->bind('env', fn () => $env);
 }
 
 /**
  * Creates a sandbox in which the base path is updated.
  */
-function sandbox(callable $callback, string $base = __DIR__): string
+function in_sandbox(callable $callback, string $base = __DIR__): string
 {
     return tap($base . '/__sandbox__/' . Str::random(), function (string $directory) use ($callback) {
         $initialBasePath = base_path();
         App::setBasePath($directory);
-        File::makeDirectory($directory, 0755, true);
+        File::makeDirectory($directory, recursive: true);
         $callback($directory);
         File::deleteDirectory($directory);
         App::setBasePath($initialBasePath);
     });
-}
-
-/**
- * Starts the fake development server.
- */
-function start_dev_server(): void
-{
-    if (optional(this()->server)->isRunning()) {
-        return;
-    }
-
-    this()->server = new Process([
-        'php',
-        '-S',
-        'localhost:3000',
-        $directory = __DIR__ . '/Server',
-        $directory . '/index.php',
-    ]);
-
-    this()->server->start();
-    this()->server->waitUntil(fn ($_, $output) => str_contains($output, 'started'));
-}
-
-/**
- * Stops the fake development server.
- */
-function stop_dev_server(): void
-{
-    if (optional(this()->server)->isRunning()) {
-        this()->server->stop();
-    }
-}
-
-/**
- * Calls the given callback after a fake development server has been started.
- */
-function with_dev_server(callable $callback): void
-{
-    start_dev_server();
-    $callback();
-    stop_dev_server();
 }
