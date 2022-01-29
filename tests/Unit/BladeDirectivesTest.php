c3093b71481bb96187a09d00bbfaf57c5cbd4049
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-29 21:19:08 +0100

    test: cover blade directives

--- tests/Unit/BladeDirectivesTest.php
+++ tests/Unit/BladeDirectivesTest.php
@@ -3,30 +3,35 @@
 use Illuminate\Support\Facades\Blade;
 
 beforeEach(function () {
-    test()->directives = Blade::getCustomDirectives();
+    this()->directives = Blade::getCustomDirectives();
 });
 
-it('creates directives', function () {
-    expect(test()->directives)
-        ->toHaveKeys(['vite', 'client']);
+it('creates directives the vite, tag, client and react directives', function () {
+    expect(this()->directives)
+        ->toHaveKeys(['vite', 'tag', 'client', 'react']);
 });
 
-it('generates a call to vite_tags() from the @vite directive when there is no parameters', function () {
-    expect(test()->directives['vite']())
-        ->toBe('<?php echo vite_tags() ?>');
+it('generates a call to vite_tags() with the default configuration from the @vite directive when there is no parameters', function () {
+    expect(this()->directives['vite']())
+        ->toBe('<?php echo vite_tags(e("default")); ?>');
 });
 
-it('generates a call to vite_entry() from the @vite directive when there is a parameter', function () {
-    expect(test()->directives['vite']('resources/ts/main.ts'))
-        ->toBe('<?php echo vite_entry(e(resources/ts/main.ts)); ?>');
+it('generates a call to vite_tag() with the default configuration from the @vite directive when there is a parameter', function () {
+    expect(this()->directives['tag']('"main"'))
+        ->toBe('<?php echo vite_tag(e("main"), e("default")); ?>');
+});
+
+it('generates a call to vite_tag() with the given configuration from the @tag directive when there is two parameters', function () {
+    expect(this()->directives['tag']('"main", "config-name"'))
+        ->toBe('<?php echo vite_tag(e("main"), e("config-name")); ?>');
 });
 
 it('generates a call to vite_client() from the @client directive', function () {
-    expect(test()->directives['client']())
+    expect(this()->directives['client']())
         ->toBe('<?php echo vite_client(); ?>');
 });
 
 it('generates a call to vite_react_refresh_runtime() from the @react directive', function () {
-    expect(test()->directives['react']())
+    expect(this()->directives['react']())
         ->toBe('<?php echo vite_react_refresh_runtime(); ?>');
 });
