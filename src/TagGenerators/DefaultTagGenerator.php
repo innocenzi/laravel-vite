d88b22b44af36a7203a22421a7bc711d5b215490
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-05-13 18:03:12 +0800

    Style: compress script

--- src/TagGenerators/DefaultTagGenerator.php
+++ src/TagGenerators/DefaultTagGenerator.php
@@ -18,31 +18,12 @@ final class DefaultTagGenerator implements TagGenerator
 
     public function makeLegacyScriptTag(string $url, Chunk $chunk = null): string
     {
-        if (Str::contains($chunk?->file, 'polyfills-legacy')) {
+        if (Str::contains($chunk?->src, 'vite/legacy-polyfills')) {
             $safariFix = '<script nomodule>!function(){var e=document,t=e.createElement("script");if(!("noModule"in t)&&"onbeforeload"in t){var n=!1;e.addEventListener("beforeload",(function(e){if(e.target===t)n=!0;else if(!e.target.hasAttribute("nomodule")||!n)return;e.preventDefault()}),!0),t.type="module",t.src=".",e.head.appendChild(t),t.remove()}}();</script>';
-
-            $bundleLoader = <<<HTML
-            <script type="module">
-                !function(){
-                    try {
-                        new Function("m","return import(m)")
-                    } catch(o) {
-                        console.warn("vite: loading legacy build because dynamic import is unsupported, syntax error above should be ignored");
-                        var e=document.getElementById("vite-legacy-polyfill"), n=document.createElement("script");
-                        n.src=e.src, n.onload=function(){
-                            var entries = Array.prototype.slice.call(document.querySelectorAll('[data-vite-legacy]'), 0);
-                            entries.forEach(function (entry) {
-                                System.import(entry.getAttribute('data-src')).catch(console.error)
-                            });
-                        },
-                        document.body.appendChild(n);
-                    }
-                }();
-            </script>
-            HTML;
-
+            $bundleLoader = '<script type="module">!function(){try{new Function("m","return import(m)")}catch(o){console.warn("vite: loading legacy build because dynamic import is unsupported, syntax error above should be ignored");var e=document.getElementById("vite-legacy-polyfill"),n=document.createElement("script");n.src=e.src,n.onload=function(){var entries=Array.prototype.slice.call(document.querySelectorAll("[data-vite-legacy]"),0);entries.forEach(function(entry){System.import(entry.getAttribute("data-src")).catch(console.error)})},document.body.appendChild(n)}}();</script>';
             $legacyBundle = sprintf('<script nomodule id="vite-legacy-polyfill" src="%s"></script>', $url);
-            return implode("\n", [$safariFix, $bundleLoader, $legacyBundle]);
+
+            return implode("\r\n", [$safariFix, $bundleLoader, $legacyBundle]);
         }
 
         return sprintf('<script nomodule src="%s"%s></script>', $url, $this->processIntegrity($chunk));
