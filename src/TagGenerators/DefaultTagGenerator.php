c04bbc5145c525814ea9b193e4c7e358dbdbc5e7
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-05-13 18:06:33 +0800

    Style: apply php-cs-fixer

--- src/TagGenerators/DefaultTagGenerator.php
+++ src/TagGenerators/DefaultTagGenerator.php
@@ -46,7 +46,7 @@ final class DefaultTagGenerator implements TagGenerator
         ];
 
         return ' ' . collect($attributes)
-                ->map(fn($value, $key) => sprintf('%s="%s"', $key, $value))
+                ->map(fn ($value, $key) => sprintf('%s="%s"', $key, $value))
                 ->join(' ');
     }
 }
