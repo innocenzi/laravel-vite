964c55724c3a36e713f71819052d550bc5bbe067
Enzo Innocenzi <enzo@innocenzi.dev>
2022-06-01 12:45:25 +0200

    feat(vite): export `inertia` helpers

--- vite-plugin-laravel/tsup.config.ts
+++ vite-plugin-laravel/tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from 'tsup'
 export const tsup: Options = {
 	clean: true,
 	dts: true,
-	format: ['cjs'],
+	format: ['cjs', 'esm'],
 	external: ['rollup', 'vite', 'esbuild', 'tailwindcss', 'autoprefixer'],
 	noExternal: ['execa'],
 }
