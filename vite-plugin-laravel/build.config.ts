0d55aaa2bd14366d7daad83db190e1d830ee955b
Enzo Innocenzi <enzo@innocenzi.dev>
2022-06-03 14:38:48 +0200

    feat: move from `tsup` to `unbuild`

---
+++ vite-plugin-laravel/build.config.ts
@@ -0,0 +1,23 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+	entries: [
+		'src/index',
+		'src/inertia',
+	],
+	clean: true,
+	declaration: true,
+	externals: [
+		'rollup',
+		'vite',
+		'esbuild',
+		'tailwindcss',
+		'autoprefixer',
+		'chalk',
+	],
+	rollup: {
+		emitCJS: true,
+		cjsBridge: true,
+		inlineDependencies: true,
+	},
+})
--- vite-plugin-laravel/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Options } from 'tsup'
-
-export const tsup: Options = {
-	clean: true,
-	dts: true,
-	format: ['cjs', 'esm'],
-	external: ['rollup', 'vite', 'esbuild', 'tailwindcss', 'autoprefixer'],
-	noExternal: ['execa'],
-}
