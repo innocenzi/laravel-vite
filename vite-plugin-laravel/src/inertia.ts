799a0712ce0df394dab1cd1313e592cf6a1a4abb
Enzo Innocenzi <enzo@innocenzi.dev>
2022-08-04 14:25:43 +0200

    feat(vite/inertia): support default layouts in resolvePageComponent

--- vite-plugin-laravel/src/inertia.ts
+++ vite-plugin-laravel/src/inertia.ts
@@ -1,16 +1,21 @@
 /**
  * Resolves a page component.
  */
-export async function resolvePageComponent(name: string, pages: Record<string, any>) {
+export async function resolvePageComponent(name: string, pages: Record<string, any>, defaultLayout?: any) {
 	const path = Object.keys(pages)
 		.sort((a, b) => a.length - b.length)
 		.find((path) => path.endsWith(`${name.replaceAll('.', '/')}.vue`))
 
 	if (!path) {
-		throw new Error(`Page not found: ${name}`)
+		throw new Error(`Page component "${name}" could not be found.`)
 	}
 
-	return typeof pages[path] === 'function'
+	let component = typeof pages[path] === 'function'
 		? await pages[path]()
 		: pages[path]
+
+	component = component.default ?? component
+	component.layout ??= defaultLayout
+
+	return component
 }
